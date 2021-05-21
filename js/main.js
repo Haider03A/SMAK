7fcc7777526c63dbcb8ce3c2f545a971a765a7ab
hayderali3612 <hayderali3612@gmail.com>
2021-05-21 18:56:19 +0300

    Finish Header And Home

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/* var but = document.querySelector(".gitBut");
+var iconShow = document.querySelector(".content");
+var iconLeft = document.querySelector(".content i:first-child");
+var iconRight = document.querySelector(".content i:last-child");
+
+
+
+but.onmouseover = function() {
+
+    but.className = "gitBut borderDisplay";
+
+};
+but.onmouseout = function() {
+
+    but.className = "gitBut bordernone";
+
+};
+
+iconShow.onmouseover = function() {
+
+    iconLeft.className = "icon-angle-left show";
+    iconRight.className = "icon-angle-right show";
+};
+iconShow.onmouseout = function() {
+
+    iconLeft.className = "icon-angle-left";
+    iconRight.className = "icon-angle-right";
+
+}; */
