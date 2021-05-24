a45a10a47eaa99fe56333b75eab8767ab2d8e4f3
hayderali3612 <hayderali3612@gmail.com>
2021-05-25 00:14:57 +0300

    Full Fianish Header Home

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 var but = document.querySelector(".gitBut"),
-    textAndIcon = document.querySelector(".textAndIcon"),
-    moveLeft = document.querySelector(".home .content .textAndIcon i:first-child"),
-    moveRight = document.querySelector(".home .content .textAndIcon i:last-child"),
-    bergherIcon = document.querySelector("header .icon"),
-    showNav = document.querySelector("header .nav");
+textAndIcon = document.querySelector(".textAndIcon"),
+disBlock = document.querySelectorAll(".textAndIcon i:first-child, .textAndIcon i:last-child")
+bergherIcon = document.querySelector("header .icon"),
+showNav = document.querySelector("header .nav");
 
 bergherIcon.onclick = function() {
 
-    showNav.classList.toggle("show");
+  showNav.classList.toggle("show");
 
-    if (bergherIcon.className == "icon-bars icon") {
-        bergherIcon.classList.remove("icon-bars")
-        bergherIcon.classList.add("icon-x");
-    } else {
-        bergherIcon.className = "icon-bars icon";
-    };
+  if (bergherIcon.classList.contains("icon-bars")) {
+    bergherIcon.classList.remove("icon-bars")
+    bergherIcon.classList.add("icon-x");
+  } else {
+    bergherIcon.classList.add('icon-bars');
+  };
 
 };
 
 but.onmouseover = function() {
-        but.classList.add("borderDisplay");
-    },
+  but.classList.add("borderDisplay");
+},
 
-    but.onmouseout = function() {
-        but.classList.remove("borderDisplay");
-    },
+but.onmouseout = function() {
+  but.classList.remove("borderDisplay");
+};
 
-    textAndIcon.onmouseover = function() {
-        moveLeft.classList.add("showLeft");
-        moveRight.classList.add("showRight");
-    },
+textAndIcon.onmouseover = function() {
+        disBlock[0].classList.add('show');
+        disBlock[1].classList.add('show');
+    };
 
     textAndIcon.onmouseout = function() {
-        moveLeft.classList.remove("showLeft");
-        moveRight.classList.remove("showRight");
-    };
+        disBlock[0].classList.remove('show');
+        disBlock[1].classList.remove('show');
+};
+
