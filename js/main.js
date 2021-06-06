dacab273f99561d8d6cea2f2719645a96d537956
hayderali3612 <hayderali3612@gmail.com>
2021-06-06 11:17:06 +0300

    d

--- js/main.js
+++ js/main.js
@@ -1,37 +1,49 @@
-var but = document.querySelector(".gitBut"),
-textAndIcon = document.querySelector(".textAndIcon"),
-disBlock = document.querySelectorAll(".textAndIcon i:first-child, .textAndIcon i:last-child")
-bergherIcon = document.querySelector("header .icon"),
-showNav = document.querySelector("header .nav");
+const but = document.querySelector(".gitBut"),
+    butDonw = document.querySelector(".but-down"),
+    textAndIcon = document.querySelector(".textAndIcon"),
+    disBlock = document.querySelectorAll(".textAndIcon i:first-child, .textAndIcon i:last-child"),
+    bergherIcon = document.querySelector("header .berger-icon"),
+    showNav = document.querySelector("header .nav"),
+    links = document.querySelectorAll("header .nav li");
 
 bergherIcon.onclick = function() {
 
-  showNav.classList.toggle("show");
+    showNav.classList.toggle('show');
+    bergherIcon.classList.toggle('berger-toggle');
 
-  if (bergherIcon.classList.contains("icon-bars")) {
-    bergherIcon.classList.remove("icon-bars")
-    bergherIcon.classList.add("icon-x");
-  } else {
-    bergherIcon.classList.add('icon-bars');
-  };
+    links.forEach((links, index) => {
+        if (links.style.animation) {
+            links.style.animation = '';
+        } else {
+            links.style.animation = `link-move 500ms ${index / 7 + 0.5}s`;
+        }
+    });
+
 
 };
 
 but.onmouseover = function() {
-  but.classList.add("borderDisplay");
-},
+        but.classList.add("borderDisplay");
+    },
 
-but.onmouseout = function() {
-  but.classList.remove("borderDisplay");
-};
-
-textAndIcon.onmouseover = function() {
-        disBlock[0].classList.add('show');
-        disBlock[1].classList.add('show');
+    but.onmouseout = function() {
+        but.classList.remove("borderDisplay");
     };
 
-    textAndIcon.onmouseout = function() {
-        disBlock[0].classList.remove('show');
-        disBlock[1].classList.remove('show');
+textAndIcon.onmouseover = function() {
+    disBlock[0].classList.add('show');
+    disBlock[1].classList.add('show');
 };
 
+textAndIcon.onmouseout = function() {
+    disBlock[0].classList.remove('show');
+    disBlock[1].classList.remove('show');
+};
+
+butDonw.onmouseover = function() {
+    butDonw.classList.add("border-down");
+};
+
+butDonw.onmouseout = function() {
+    butDonw.classList.remove("border-down");
+};
